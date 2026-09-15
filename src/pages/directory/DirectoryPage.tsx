4687fd98e839a5e90bc512c7acbdbea681f7d2e4
TasneemJarrar <tasneem.a.jarrar@gmail.com>
2026-09-15 11:04:03 +0300

    fix: remove unused react declaration

--- src/pages/directory/DirectoryPage.tsx
+++ src/pages/directory/DirectoryPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function DirectoryPage() {
   return (
     <div>DirectoryPage</div>
--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function OnboardingPage() {
   return (
     <div>OnboardingPage</div>
